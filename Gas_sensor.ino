c6d889239992a49bb00d8092b69dec27db77f049
John McLear <john@mclear.co.uk>
2012-10-30 21:45:35 +0000

    updated

--- Gas_sensor.ino
+++ Gas_sensor.ino
@@ -1,12 +1,3 @@
-/* A gas sensor with the following features */
-/* 
-3 States.. OK, Warning, Alert
-2 lines on warning.
-Plays a melody on Alert.
-Debouncing to stop flickering fan on / off
-*/
-
-
 #include "pitches.h"
 #include "Bounce.h"
 
@@ -16,8 +7,10 @@ int greenLed = 3; // teh LED port
 int warningLed = 7; // the LED port
 int redLed = 4; // teh LED port
 int speaker = 8; // Speaker
+int extractorFan = 13; // Extractor fan Solid State port - Used for switching on/off an extractor fan
 int lastButtonState = LOW;   // the previous reading from the input pin
 int buttonState;             // the current reading from the input pin
+int playTone = false; // Set to true to play a tone
 
 
 // notes in the melody:
@@ -57,33 +50,38 @@ void loop() {
       digitalWrite(greenLed, HIGH);    // turn the LED off by making the voltage LOW
       digitalWrite(redLed, LOW);
       digitalWrite(warningLed, HIGH);
+      digitalWrite(extractorFan, HIGH); // Turn on the extractor fan
     }
     else if(val > 100){
       digitalWrite(greenLed, LOW);    // turn the LED off by making the voltage LOW
       digitalWrite(redLed, HIGH);
       digitalWrite(warningLed, HIGH);
+      digitalWrite(extractorFan, HIGH); // Turn on the extractor fan
 
-      // iterate over the notes of the melody:
-      for (int thisNote = 0; thisNote < 8; thisNote++) {
-
-        // to calculate the note duration, take one second 
-        // divided by the note type.
-        //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-        int noteDuration = 1000/noteDurations[thisNote];
-        tone(speaker, melody[thisNote],noteDuration);
-
-        // to distinguish the notes, set a minimum time between them.
-        // the note's duration + 30% seems to work well:
-        int pauseBetweenNotes = noteDuration * 1.30;
-        delay(pauseBetweenNotes);
-        // stop the tone playing:
-        noTone(speaker);
+      if(playTone === true){  // If the playTone Setting is set)
+        // iterate over the notes of the melody:
+        for (int thisNote = 0; thisNote < 8; thisNote++) {
+  
+          // to calculate the note duration, take one second 
+          // divided by the note type.
+          //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+          int noteDuration = 1000/noteDurations[thisNote];
+          tone(speaker, melody[thisNote],noteDuration);
+  
+          // to distinguish the notes, set a minimum time between them.
+          // the note's duration + 30% seems to work well:
+          int pauseBetweenNotes = noteDuration * 1.30;
+          delay(pauseBetweenNotes);
+          // stop the tone playing:
+          noTone(speaker);
+        }
       }
     }
     else{ // everyting okay.
       digitalWrite(greenLed, HIGH); 
       digitalWrite(redLed, LOW); 
       digitalWrite(warningLed, LOW);
+      digitalWrite(extractorFan, LOW); // Turn off the extractor fan
     }
     buttonState = val;
   }
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 ardiono_gas_sensor
 ==================
 
-Arduino Gas Sensor
+A gas sensor with the following features
+
+* Extractor fan On/Off control
+* Debouncing to stop flickering fan on / off
+* 3 States.. OK, Warning, Alert
+* 2 lights on warning.
+* Optional Melody on Alert.
